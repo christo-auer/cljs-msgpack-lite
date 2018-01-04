332f38e0c0c5dc9f5d6cc434ba71c6460dfa2df8
Christopher Auer <christopher.auer@web.de>
2018-01-04 14:47:31 +0100

    added doo for testing and spec for arg checks

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-msgpack-lite "0.1.4"
+(defproject cljs-msgpack-lite "0.1.5"
   :description "cljs-msgpack-lite is a lightweight and convenient wrapper around msgpack-lite for ClojureScript."
   :url "https://github.com/christo-auer/cljs-msgpack-lite"
 
@@ -6,16 +6,18 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [camel-snake-kebab "0.4.0"]
+                 [org.clojure/test.check "0.10.0-alpha2"]
                  [org.clojure/clojurescript "1.9.908"]]
 
   :plugins [[lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
             [lein-figwheel "0.5.13"]
+            [lein-doo "0.1.8"]
             [lein-npm "0.6.2"]]
 
   :npm {:dependencies [[msgpack-lite "0.1.26"]]
         :devDependencies [[ws "3.3.2"]]}
 
-  :source-paths ["src"]
+  :source-paths ["src" "test"]
 
   :clean-targets ["server.js"
                   "target"]
@@ -32,6 +34,13 @@
                 :target :nodejs
                 :optimizations :none
                 :source-map-timestamp true}}
+             {:id "test"
+              :source-paths ["src" "test"]
+              :compiler {
+                         :main cljs-msgpack-lite.runner
+                         :output-to "target/js/compiled/cljs_msgpack_lite_test.js"
+                         :target :nodejs
+                         :optimizations :none}}             
              {:id "prod"
               :source-paths ["src"]
               :compiler {
